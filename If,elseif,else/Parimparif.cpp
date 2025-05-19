c924d578fb6baff9d897499711dd6707b5815426
Fernando18e6 <00137925@uca.edu.sv>
2025-05-19 06:09:52 +0200

    end:white_check_mark:

---
+++ If,elseif,else/Parimparif.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main(){
+    //Se declaran variables.
+    int num=0;
+
+    cout<<"Put a number ";
+    cin>>num;
+
+    if (num % 2 == 0){
+        cout<<"Su numero es par. ";
+    }
+    else{
+
+        cout<<"Su numero es impar";
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    return 0;
+}
