947f2b098f187391e08ee1b28ee5bd65a507a5b1
Fernando18e6 <00137925@uca.edu.sv>
2025-05-19 05:42:45 +0200

    .\Edadif.cpp:white_check_mark:

--- If,elseif,else/Edadif.cpp
+++ If,elseif,else/Edadif.cpp
@@ -1,27 +1,22 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-    //The variables are declared
+int main()
+{
+    // The variables are declared.
     int age;
 
-    //Age is requested
-    cout<< "Put your age. "
-    cin>>age;
+    // Age is requested.
+    cout << "Put your age. ";
+    cin >> age;
 
-    if (age<18){
-        cout<<"You are a minor.";
+    // It is checked whether they are underage or of legal age.
+    if (age < 18)
+    {
+        cout << "You are a minor.";
     }
-    else {
-        cout<<"You are of legal age. "
-
-    }
-
-
-
-
+    else
+        cout << "You are of legal age. ";
 
     return 0;
-
-
 }
