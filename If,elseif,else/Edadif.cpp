1dc9d4c1ad995517c8f92d7bb571371cfbee27fa
Fernando18e6 <00137925@uca.edu.sv>
2025-05-19 04:26:11 +0200

    .\Edadif.cpp:sparkles:

---
+++ If,elseif,else/Edadif.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    //The variables are declared
+    int age;
+
+    //Age is requested
+    cout<< "Put your age. "
+    cin>>age;
+
+    if (age<18){
+        cout<<"You are a minor.";
+    }
+    else {
+        cout<<"You are of legal age. "
+
+    }
+
+
+
+
+
+    return 0;
+
+
+}
